5ab102dd30616f7ef18d34c42016bb95ca784be4
aalamaro <75432734+aalamaro@users.noreply.github.com>
2023-05-22 11:09:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 #streamlit.markdown("this is the markdown")
 #streamlit.subheader("this is the subheader")
 #streamlit.caption("this is the caption")
