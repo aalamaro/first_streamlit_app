a67cde88ced65efa916482282a90663166bf1675
aalamaro <75432734+aalamaro@users.noreply.github.com>
2023-05-22 11:23:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-if (length(fruits_selected) > 0):
+if (len(fruits_selected) > 0):
   fruits_to_show = my_fruit_list.loc[fruits_selected]
 else:
   fruits_to_show = my_fruit_list
