fdbe02f61efb647f9bfb5226eb193af22f219369
aalamaro <75432734+aalamaro@users.noreply.github.com>
2023-05-22 11:05:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header("Breakfast Menu")
-streamlit.markdown("Omega 3 & Blueberry Oatmeal")
-streamlit.markdown("Kale, Spinach & Rocket Smoothie")
-streamlit.markdown("Hard-Boiled Free-Range Egg")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+#streamlit.markdown("this is the markdown")
 #streamlit.subheader("this is the subheader")
 #streamlit.caption("this is the caption")
